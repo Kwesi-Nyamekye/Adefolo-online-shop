6c1bca60b6e622c0d910564eb4887c84dc3507f0
Nana Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-07-09 05:24:51 +0000

    Update views for Orders

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,10 @@
+# views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order, OrderItem
 from .forms import OrderCreatedForm
 from cart.cart import Cart
+from django.urls import reverse
+from .tasks import send_order_confirmation_email
 
 def order_create(request):
     cart = Cart(request)
@@ -9,7 +12,10 @@ def order_create(request):
     if request.method == "POST":
         form = OrderCreatedForm(request.POST)
         if form.is_valid():
+            # Save the order from the form data
             order = form.save()
+            
+            # Create OrderItem instances for each item in the cart
             for item in cart:
                 OrderItem.objects.create(
                     order=order,
@@ -17,13 +23,27 @@ def order_create(request):
                     price=item['price'],
                     quantity=item['quantity']
                 )
+            
+            # Clear the cart after creating order items
             cart.clear()
-            return redirect('orders:order_created', order_id=order.id)
+            
+            # Delay execution of the Celery task to send order confirmation email
+            send_order_confirmation_email.delay(order.id)
+            
+            # Store the order ID in session for further processing
+            request.session['order_id'] = order.id
+            
+            # Redirect to the payment processing page
+            return redirect(reverse('payment:process'))
     else:
         form = OrderCreatedForm()
     
+    # Render the order creation form
     return render(request, "orders/order_create.html", {'cart': cart, 'form': form})
 
 def order_created(request, order_id):
+    # Retrieve the order details based on order_id
     order = get_object_or_404(Order, id=order_id)
+    
+    # Render the order created confirmation page
     return render(request, 'orders/order_created.html', {'order': order})
